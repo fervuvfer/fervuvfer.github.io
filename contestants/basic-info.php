6848f1af69070744e82c7bb4e55ba103ccb66ba1
fervuvfer <84018360+fervuvfer@users.noreply.github.com>
2022-04-29 22:25:47 +1000

    PHP

---
+++ contestants/basic-info.php
@@ -0,0 +1,65 @@
+<div id="container" class="Key Info"><a id="keyinfo"></a>
+  <div class="letter title">
+    <h2>Key Info</2h>
+      <section>
+        <p class="task-title">Dates</p>
+          <section>
+            <p>Day 1: Local Scout Hall</p>
+            <p class="points">3/7/2022</p>
+          </section>
+          <section>
+            <p>2 Day Weekend: Regional Airbnb</p>
+            <p class="points">16/7/2022 - 17/7/2022</p>
+          </section>
+      </section>
+      <section>
+        <p class="task-title">Payment</p>
+          <section>
+            <p>$85</p>
+            <p class="points">Bank Transfer or PayID: 0403515789</p>
+          </section>
+      </section>
+  </div>
+</div>
+<div id="container" class="CoolStuff"><a id="coolshit"></a>
+  <div class="letter">
+    <p>Taskmaster 1</p>
+    <div class="video-container">
+  <iframe src="https://www.youtube.com/embed/tj_xhiObzKE" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+</div>
+  </div>
+  <div class="letter">
+    Cool Shit
+  </div>
+  <div class="letter">
+    Cool Shit
+  </div>
+  <div class="letter">
+    Cool Shit
+  </div>
+  <div class="letter">
+    Cool Shit
+  </div>
+  <div class="letter">
+    Cool Shit
+  </div>
+</div>
+<div id="container" class="faq"><a id="faq"></a>
+  <div class="letter title">
+    <h2>FAQ</2h>
+    <p class="points">Some questions answered.</p>
+  </div>
+  <div class="letter">
+    FAQ HERE
+  </div>
+  <div class="letter">
+    FAQ HERE
+  </div>
+  <div class="letter">
+    FAQ HERE
+  </div>
+  <div class="letter">
+    FAQ HERE
+  </div>
+
+</div>
